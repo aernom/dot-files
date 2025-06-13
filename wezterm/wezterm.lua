2683c688d5b395e3b4ac1bafc1dfa6534ca490f7
aernom <andrea.montorio@gmail.com>
2025-06-13 16:07:38 +0200

    no bell please!

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,13 +11,17 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 50
 
+-- TABS
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
+
 config.window_frame = {
 	inactive_titlebar_bg = "none",
 	active_titlebar_bg = "none",
 }
 
-config.tab_bar_at_bottom = true
-config.hide_tab_bar_if_only_one_tab = true
+-- BELL
+config.audible_bell = "Disabled"
 
 -- KEY BINDING
 local action = wezterm.action
